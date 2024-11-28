87a179d02cdf27e967fb98bb251d586d0e983e46
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-28 23:58:06 +0530

    refactor(App): Simplify component structure, remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import { useEffect, useState } from "react";
-import SideNavBar from "./components/SideNavBar";
-import About from "./pages/About";
 import { ThemeProvider } from "./themeContext";
-import Skills from "./pages/Skills";
+
 import Preloader from "./components/Preloader";
 import AppRoutes from "./AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-toastify/dist/ReactToastify.min.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <ThemeProvider>
-      <>
-        <div>
-          <Preloader />
-
-          <AppRoutes />
-        </div>
-      </>
+      <Preloader />
+      <Wrapper>
+        <AppRoutes />
+      </Wrapper>
       <ToastContainer />
     </ThemeProvider>
   );
