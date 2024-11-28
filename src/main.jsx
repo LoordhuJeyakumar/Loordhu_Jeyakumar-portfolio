2fa1cb7c6fcf8b08a14d18ba1ccbfbf85e724288
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-29 00:03:23 +0530

    unuse removed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./App.css";
 import "./index.css";
-import "./components/root.css";
+//import "./components/root.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.css";
