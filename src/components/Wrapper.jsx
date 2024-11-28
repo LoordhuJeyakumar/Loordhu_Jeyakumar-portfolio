d5419c96c6bcbe4e3b29a54ee56337f98d8a89e1
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-28 23:35:16 +0530

    feat: Update Wrapper component with Footer and styling

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import SideNavBar from "./SideNavBar";
 import { Outlet } from "react-router";
+import Footer from "./Footer";
 
 function Wrapper({ children }) {
   return (
-    <div className="row">
+    <div className="row bg-light-background dark:bg-dark-background">
       <div className="col-lg-9 col-12">
         {children}
         <Outlet />
+        <Footer />
       </div>
+
       <div className="col-lg-3 col-12">
         <SideNavBar />
       </div>
